0ea237ada44337fa5959af48a85b8be7fd987731
TanvirCoU <ahmeadtanvir@stud.cou.ac.bd>
2023-10-23 03:00:07 +0600

    updated

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require("cors");
+const bodyParser = require("body-parser");
 require('dotenv').config();
 const foodDataHandler = require('./routeHandler/foodDataHandler');
 const orderHandler = require('./routeHandler/orderHandler');
 
 const app = express()
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(cors());
 
 const port = 5000;
